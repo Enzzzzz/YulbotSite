14e0605274efbaa43a7a1099adef443ab7a1624f
Enzzzzz <contatoenzz@gmail.com>
2022-12-22 16:31:14 -0300

    image

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,10 +7,6 @@ import Navbar from '../Layout/Navbar'
 
 function Home() {
 
-  const direito = '../../public/direitoBranco.png'
-  const config = '../../public/config.png'
-  const up = '../../public/up.png'
-
   return (
   <div className='bg-primary w-full overflow-hidden'>
     <div className={`sm:px-16 px-6 flex justify-center items-center`}>
@@ -31,7 +27,7 @@ function Home() {
         title= "Moderação"
         description= 'Mutar, Remover e até Banir nunca foi tão fácil! Esses e muitos outros comandos para administrar seu servidor perfeitamente e com muita facilidade!'
         clickText= 'Veja os Comandos!'
-        img={direito}
+        img= '/direitoBranco.png'
         path='mod'
         altImg= 'direito'
         />
@@ -40,7 +36,7 @@ function Home() {
         title= "Sistema de Level"
         description= 'Sistema de level e rank global para todos os meus usuários ou apenas para o seu servidor! Converse com seus amigos e ganhem pontos de xp para subir de nível e liberar novos temas para o seu perfil ou até comandos.'
         clickText= 'Saiba mais!'
-        img={up}
+        img= '/up.png'
         path='level'
         altImg= 'level'
         />
@@ -49,7 +45,7 @@ function Home() {
         title= "Totalmente Configurável"
         description= 'Você pode me configurar do jeito que voce quiser! setar canais de comando, setar canal de log adm, bloquear comandos em chat específico, bloquear um usuário ou cargo de usar um comando específico e ainda um pouco mais!'
         clickText= 'Veja Como!'
-        img={config}
+        img= '/config.png'
         path='config'
         altImg= 'config'
         />
