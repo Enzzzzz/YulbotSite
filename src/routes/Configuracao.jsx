b981ba7d6abaf303fa661f08f5b2f4b055efc488
Enzzzzz <contatoenzz@gmail.com>
2023-06-04 14:47:47 -0300

    a

--- src/routes/Configuracao.jsx
+++ src/routes/Configuracao.jsx
@@ -5,50 +5,53 @@ import Navbar from '../Layout/Navbar'
 import React, { useEffect } from 'react';
 
 
-useEffect(() => {
-  window.fbAsyncInit = function () {
-    FB.init({
-      appId: "773538684462895",
-      cookie: true,
-      xfbml: true,
-      version: "v17.0",
-    });
 
-    FB.AppEvents.logPageView();
-  };
-
-  (function (d, s, id) {
-    var js,
-      fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-      return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  })(document, "script", "facebook-jssdk");
-
-  // Função para lidar com o status de login
-  function statusChangeCallback(response) {
-    // Sua lógica de tratamento de resposta aqui
-  }
-
-  // Função para verificar o status de login ao carregar a página
-  function checkLoginStatus() {
-    FB.getLoginStatus(function (response) {
-      statusChangeCallback(response);
-    });
-  }
-
-  // Executar a função quando a página é carregada
-  window.onload = function () {
-    checkLoginStatus();
-  };
-}, []);
 
 
 function Configuracao() {
+
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      FB.init({
+        appId: "773538684462895",
+        cookie: true,
+        xfbml: true,
+        version: "v17.0",
+      });
+  
+      FB.AppEvents.logPageView();
+    };
+  
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  
+    // Função para lidar com o status de login
+    function statusChangeCallback(response) {
+      // Sua lógica de tratamento de resposta aqui
+    }
+  
+    // Função para verificar o status de login ao carregar a página
+    function checkLoginStatus() {
+      FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+      });
+    }
+  
+    // Executar a função quando a página é carregada
+    window.onload = function () {
+      checkLoginStatus();
+    };
+  }, []);
+
     return (
     <div className={`bg-primary w-full overflow-hidden'`}>
         <div className={`sm:px-16 px-6 flex justify-center items-center`}>
