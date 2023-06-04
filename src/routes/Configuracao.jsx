a5fbef97e49ad1ddf832b8d3f3d237365087bc78
Enzzzzz <contatoenzz@gmail.com>
2023-06-04 16:22:50 -0300

    a

--- src/routes/Configuracao.jsx
+++ src/routes/Configuracao.jsx
@@ -3,52 +3,12 @@ import Footer from '../Layout/Footer';
 import Navbar from '../Layout/Navbar';
 
 function Configuracao() {
-
-  window.fbAsyncInit = function () {
-    FB.init({
-      appId: "773538684462895",
-      cookie: true,
-      xfbml: true,
-      version: "v17.0",
-    });
-
-    FB.AppEvents.logPageView();
-  };
-
-  (function (d, s, id) {
-    var js,
-      fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-      return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  })(document, "script", "facebook-jssdk");
-
-  function statusChangeCallback(response) {
-    console.log(response);
-  }
-
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-    console.log(response)
-});
-
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className="sm:px-16 px-6 flex justify-center items-center">
         <div className="xl:max-w-[1280px] w-full">
           <Navbar />
         </div>
-      </div>
-
-      <div className="fb-login-button" scope="public_profile,email" onlogin="checkLoginStatus();">
-      <fb:login-button 
-  scope="public_profile,email"
-  onlogin="checkLoginState();">
-</fb:login-button>
       </div>
 
       <div>
