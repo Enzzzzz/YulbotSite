14e44461085c0f95df539565bafe44ba0b723bc1
Enzzzzz <contatoenzz@gmail.com>
2023-12-27 17:21:48 -0300

    Old commands has been disabled

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -5,7 +5,7 @@ import Footer from '../Layout/Footer'
 
 function Sobre() {
     return (
-    <div className={`bg-primary w-full h-full min-h-screen overflow-hidden'`}>
+    <div className={`bg-primary w-full h-full min-h-screen overflow-hidden`}>
         <div className={`sm:px-16 px-6 flex justify-center items-center`}>
             <div className={`xl:max-w-[1280px] w-full`}>
                 <Navbar />
